e872ba55abf05e65be6de33c476f9a48c562c253
Aaron Nall <aaron.nall@clearobject.com>
2024-03-21 15:58:15 -0400

    As good as it'll get for now.

--- pkgs/by-name/li/livekit-ingress/package.nix
+++ pkgs/by-name/li/livekit-ingress/package.nix
@@ -1,4 +1,5 @@
-{ lib, buildGoModule, fetchFromGitHub, pkg-config, glib, gobject-introspection, gst_all_1 }:
+{ lib, buildGoModule, fetchFromGitHub, pkg-config, glib, gobject-introspection
+, gst_all_1 }:
 
 buildGoModule rec {
   pname = "livekit-ingress";
@@ -11,12 +12,27 @@ buildGoModule rec {
     hash = "sha256-d5H8MEVL0xNyzufV1lKLu3TS/usw5bCPlfXAjdcqhII=";
   };
 
-  nativeBuildInputs = [ pkg-config glib.dev gobject-introspection.dev ];
-  buildInputs = [ gst_all_1.gstreamer.dev gst_all_1.gst-plugins-base.dev ];
+  nativeBuildInputs = [
+    pkg-config
+    glib.dev
+    gobject-introspection.dev
+    gst_all_1.gst-plugins-base
+  ];
+  buildInputs = [
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+  ];
 
   vendorHash = "sha256-IPNRKXsSoTXVqq6pcp4QVepxim+7eDa8qh5b1No6DLE=";
 
   subPackages = [ "cmd/server" ];
+  postPatch = ''
+    substituteInPlace pkg/service/cmd.go \
+        --replace-fail "exec.Command(\"ingress\""  "exec.Command(\"livekit-ingress\""
+  '';
 
   postInstall = ''
     mv $out/bin/server $out/bin/livekit-ingress
